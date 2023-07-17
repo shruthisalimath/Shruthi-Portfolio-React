c0f0a6fdc3aa8141b2fcac646ad2cb352d5b342e
shru <shruthishekar@gmail.com>
2023-07-17 15:51:35 -0600

    added footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { FaGithub, FaLinkedin, FaPhone } from "react-icons/fa6";
+import { MdOutlineEmail } from "react-icons/md";
 
-
-function Footer(){
-    return(
-    <footer className="footer">
-     footer
-    </footer>
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className='footerLink' >
+                <a href='https://github.com/shruthisalimath' target='_blank' rel='noreferrer'><FaGithub /></a>
+                <a href='shruthisalimath' target='_blank' rel='noreferrer'><FaLinkedin /></a>
+                <div className='Number'> <FaPhone />111-222-3333</div>
+                <div className='Email'> <MdOutlineEmail />shruthi@gmail.com</div>
+            </div>
+        </footer>
     )
 }
 
