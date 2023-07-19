bc70fd39a0fd15ff7cc8b9dc4addffedb2c76fa1
shru <shruthishekar@gmail.com>
2023-07-19 10:44:59 -0600

    Added data for projects

--- src/components/Data/projectData.js
+++ src/components/Data/projectData.js
@@ -8,10 +8,6 @@ import WorkDayScheduler from "../../assets/images/Screen-Shot-Work-Day-Scheduler
 import codeTest from "../../assets/images/screenShot-codeTest.png";
 import nationalPark from "../../assets/images/Screenshot-national-park.png";
 import noteTaker from "../../assets/images/Screenshot-noteTaker.png";
-//import passwordGenerator from "../../assets/images/Password-generator.png";
-//import passwordGenerator from "../../assets/images/Password-generator.png";
-
-
 
 const projectList = [
     {
@@ -27,15 +23,15 @@ const projectList = [
         url: "https://vast-beach-72570.herokuapp.com/",
         image: CareConnect,
         description: "Care Connect is a full stack web application that enables users to manage employee information within an organization.",
-        technologies: "Node.js,Express.js,Handlebars.js,MySQL,Sequuelize,Chart.js",
+        technologies: "MySQL, Express.js, Node.js, Handlebars.js, CSS",
         GithubRepo: "https://https://github.com/pdangerfield/CareConnect.com/shruthisalimath/Secure-key-generator"
     },
     {
         title: "Weather_Dashboard",
         url: "https://shruthisalimath.github.io/Weather-Panel/",
         image: Weather_Dashboard,
-        description: "Weather_Dashboard features dynamically updated HTML and CSS.",
-        technologies: "HTML,CSS,JAVASCRIPT",
+        description: " weather dashboard application to get current weather conditions of city and 5 day forecast of that city.",
+        technologies: "Javascript, JQuery, Bootstrap, HTML, CSS",
         GithubRepo: "https://github.com/shruthisalimath/Weather-Panel"
     },
     {
@@ -49,52 +45,44 @@ const projectList = [
     
     {
         title: "WorkDayScheduler",
-        url: "https://shruthisalimath.github.io/Secure-key-generator/",
+        url: "https://shruthisalimath.github.io/Work-Day-Timetable/",
         image: WorkDayScheduler,
-        description: "Password generator using javascript",
-        technologies: "HTML,CSS,JAVASCRIPT",
-        GithubRepo: "https://github.com/shruthisalimath/Secure-key-generator"
+        description: "Simple calendar application that allows the user to save events for each hour of the day from 9AM to 5 PM.",
+        technologies: "JQuery, Javascript, Moment.js, Bootstrap, HTML, CSS",
+        GithubRepo: "https://github.com/shruthisalimath/Work-Day-Timetable"
     },
     {
         title: "codeTest",
-        url: "https://shruthisalimath.github.io/Secure-key-generator/",
+        url: "https://shruthisalimath.github.io/Code-knowledge-test/",
         image: codeTest,
-        description: "Password generator using javascript",
-        technologies: "HTML,CSS,JAVASCRIPT",
-        GithubRepo: "https://github.com/shruthisalimath/Secure-key-generator"
+        description: "A timer-based quiz application that stores high scores of multiple users. Answering incorrectly results in penality of time.",
+        technologies: "Javascript, Bootstrap, Browser LocalStorage, HTML, CSS",
+        GithubRepo: "https://github.com/shruthisalimath/Code-knowledge-test"
     },
     {
         title: "nationalPark",
-        url: "https://shruthisalimath.github.io/Secure-key-generator/",
+        url: " https://shruthisalimath.github.io/Team-Shrek/",
         image: nationalPark,
-        description: "Password generator using javascript",
-        technologies: "HTML,CSS,JAVASCRIPT",
-        GithubRepo: "https://github.com/shruthisalimath/Secure-key-generator"
+        description: "",
+        technologies: "Html,Css,Bulma,Javascript,Jquery",
+        GithubRepo: "https://github.com/shruthisalimath/Team-Shrek"
     },
     {
         title: "noteTaker",
-        url: "https://shruthisalimath.github.io/Secure-key-generator/",
+        url: " https://note-register.herokuapp.com/",
         image: noteTaker,
-        description: "Password generator using javascript",
-        technologies: "HTML,CSS,JAVASCRIPT",
-        GithubRepo: "https://github.com/shruthisalimath/Secure-key-generator"
+        description: "This is a simple Note Taker application that allows users to add ,view saved notes and also delete the notes if the user don't need that note anymore.",
+        technologies: "Express.js, Node.js, UUID, Javascript, HTML, CSS",
+        GithubRepo: "https://github.com/shruthisalimath/Note-Register"
     },
     {
         title: "LogoGenerator",
         url: "https://shruthisalimath.github.io/Secure-key-generator/",
         image: LogoGenerator,
-        description: "Password generator using javascript",
-        technologies: "HTML,CSS,JAVASCRIPT",
+        description: "The application enables users to generate a logo file with the input of text(3 characters),text color,choice of shape(circle, triangle and square) and shape color",
+        technologies: "Html,Css,Javascript,Node.js",
         GithubRepo: "https://github.com/shruthisalimath/Secure-key-generator"
     },
-    
-
-
 ]
-
-
-
-
-
 
 export default projectList;
